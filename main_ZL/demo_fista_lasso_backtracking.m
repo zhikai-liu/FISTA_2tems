9d00b3b6cc6af765bc3aa60cda9d33df5c6521dd
zhikai <zhikai@Zion.local>
2018-07-09 16:13:20 -0500

    add local maxima

--- main_ZL/demo_fista_lasso_backtracking.m
+++ main_ZL/demo_fista_lasso_backtracking.m
@@ -1,42 +1,30 @@
 load('all_traces_padded.mat');
 load('EPSC_templates.mat');
-Y=smooth(data_pad'-mean(data_pad));
-template1=fast_EPSC';
+Y=smooth(data_pad'-median(data_pad));
+% l=6e4:12e4;
+l=6.3e6:6.48e6;
+%l=6.48e6:6.6e6;
+signal=Y(l,1);
+template1=fast_EPSC(1:441)';
 %template2=fast_EPSC(1:length(slow_EPSC))'-slow_EPSC';
-template2=slow_EPSC';
+template2=slow_EPSC'-fast_EPSC(1:441)';
 opts.backtracking=true;
 opts.verbose=false;
-% opts.lambda1=4000;
-% opts.lambda2=9000;
-opts.pos=true;
+opts.lambda1=2*rms(signal).*max(abs(template1));
+opts.lambda2=2*rms(signal).*max(abs(template2));
+opts.pos=false;
 Xinit=[];
-%l=6.1e6:6.7e6;
-l=6e4:12e4;
+
 %[~,Xinit]=signal_deconv(Y(1:length(Y)/100,1), template,5e4,50,2000);
 %X = fista_lasso_backtracking_template(Y(l,1), template, Xinit, opts);
+[X1,X2,cost_matrix] = fista_lasso_backtracking_2tems(Y(l,1), template1,template2, Xinit,Xinit, opts);
 %% The lasso problem is: argmin( 1/2(Y-A*x1-B*x2)^2+lambda1*|x1|+lambda2*|x2|)
 
-cost_matrix=zeros(10,10);
-for i=1:10
-    for j=1:10
-        opts.lambda1=i*1000;
-        opts.lambda2=j*1000;
-        [X1,X2,cost_matrix(i,j)] = fista_lasso_backtracking_2tems(Y(l,1), template1,template2, Xinit,Xinit, opts);
-    end
-end
-% figure;
-% subplot(4,1,1)
-% plot(X1);
-% subplot(4,1,2)
-% plot(X2);
-% subplot(4,1,3)
-% plot(Y(l,1),'k')
-% hold on;
-% plot(conv(X1,template1)+conv(X2,template2),'r')
-% hold off;
-% subplot(4,1,4)
-% plot(conv(X1,template1),'b')
-% hold on;
-% plot(conv(X2,template2),'c')
-% hold off;
-% samexaxis('abc','xmt','on','ytac','join','yld',1);
+% cost_matrix=zeros(10,10);
+% for i=1:10
+%     for j=1:10
+%         opts.lambda1=i*1000;
+%         opts.lambda2=j*1000;
+%         [X1,X2,cost_matrix(i,j)] = fista_lasso_backtracking_2tems(Y(l,1), template1,template2, Xinit,Xinit, opts);
+%     end
+% end
---
+++ main_ZL/fista_local_maxima.m
@@ -0,0 +1,67 @@
+X1_recon=conv(X1,template1);
+X2_recon=conv(X2,template2);
+
+
+X1_max=get_local_maxima_above_threshold(X1,0.005,1);
+X1_X2_ratio=zeros(length(X1_max),2);
+figure;
+plot(signal);
+hold on;
+scatter(X1_max,signal(X1_max),'r*')
+figure;
+subplot(3,1,1)
+hold on;
+for i=1:length(X1_max)
+    if X1_max(i)+5<length(X1)
+        plot(X1(X1_max(i)-5:X1_max(i)+5),'k')
+        X1_X2_ratio(i,1)=sum(X1_recon(X1_max(i):X1_max(i)+50))-51*max(X1_recon(X1_max(i):X1_max(i)+50));
+    end
+end
+hold off;
+subplot(3,1,2)
+hold on;
+for i=1:length(X1_max)
+    if X1_max(i)+5<length(X2)
+        plot(X2(X1_max(i)-5:X1_max(i)+5),'k')
+         X1_X2_ratio(i,2)=sum(X2_recon(X1_max(i):X1_max(i)+50))-51*max(X2_recon(X1_max(i):X1_max(i)+50));
+    end
+end
+hold off;
+subplot(3,1,3)
+hold on;
+for i=1:length(X1_max)
+    if X1_max(i)+5<length(X2)&&X2(X1_max(i))<-0.005
+        plot(X1(X1_max(i)-5:X1_max(i)+5)+X2(X1_max(i)-5:X1_max(i)+5),'k')
+    end
+end
+figure;
+scatter(-X1_X2_ratio(:,1),-X1_X2_ratio(:,2),'k');
+chemical=-X1_X2_ratio(:,2)>-X1_X2_ratio(:,1);
+
+
+figure;
+subplot(4,1,1)
+plot(X1);
+hold on;
+scatter(X1_max(chemical),X1(X1_max(chemical)),'b')
+scatter(X1_max(~chemical),X1(X1_max(~chemical)),'g')
+hold off;
+subplot(4,1,2)
+plot(X2);
+hold on;
+scatter(X1_max(chemical),X2(X1_max(chemical)),'b')
+scatter(X1_max(~chemical),X2(X1_max(~chemical)),'g')
+hold off;
+subplot(4,1,3)
+plot(signal,'k')
+hold on;
+plot(conv(X1,template1)+conv(X2,template2),'r')
+scatter(X1_max(chemical),signal(X1_max(chemical)),'b')
+scatter(X1_max(~chemical),signal(X1_max(~chemical)),'g')
+hold off;
+subplot(4,1,4)
+plot(X1_recon,'b')
+hold on;
+plot(X2_recon,'c')
+hold off;
+samexaxis('abc','xmt','on','ytac','join','yld',1);
